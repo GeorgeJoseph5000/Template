24135355e068dd01d53bea055b453d8eb0a02096
George Joseph <71743801+GeorgeJoseph5000@users.noreply.github.com>
2022-10-11 01:31:19 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase";
 
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB7lyEspfetQfZbvtykn9urxWOhVFmh9Mg",
-    authDomain: "copticholybible.firebaseapp.com",
-    projectId: "copticholybible",
-    storageBucket: "copticholybible.appspot.com",
-    messagingSenderId: "966592209369",
-    appId: "1:966592209369:web:528a585d60a6fbc49af9c8",
-    measurementId: "G-ZFMGLSB72F"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyA9ulPLTa4O4XFVI2zHm51V-8UPdJrSPxk",
+  authDomain: "template-29050.firebaseapp.com",
+  projectId: "template-29050",
+  storageBucket: "template-29050.appspot.com",
+  messagingSenderId: "780625786954",
+  appId: "1:780625786954:web:3864d6f83f79fa5c5d8eb3",
+  measurementId: "G-S3WGH3XH83"
+};
 
 const auth = firebaseApp.auth()
 const db = firebaseApp.firestore()
